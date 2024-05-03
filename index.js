1ded11fdac31a24f6e0d3532787eac3320e8a6c5
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-05-03 09:47:58 +0200

    cimmit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const paragraph = document.getElementById('text');
 const button = document.getElementById('button');
+let currentText = 0;
 
 
 button.addEventListener('click', function() {
-
-    paragraph.textContent = 'Text has been changed dynamically!';
+    if (currentText === 0) {
+        paragraph.textContent = 'Text has been changed dynamically!';
+     currentText = 1;   
+    } else {
+        paragraph.textContent = 'Text has changed again!';
+        currentText = 0;
+    }
 });
